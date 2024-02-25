cdd7e6ed68801599374d5a29e7544141054cd36c
WennnTing <wendy1000203@gmail.com>
2024-02-25 17:05:45 +0800

    test

--- pages/index.vue
+++ pages/index.vue
@@ -5,3 +5,5 @@
 </template>
 
 <script lang="ts" setup></script>
+
+<style lang="scss"></style>
---
+++ pages/test.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+
+        <h1>test123</h1>
+    </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss"></style>
